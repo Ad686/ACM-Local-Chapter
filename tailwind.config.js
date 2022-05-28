4b14291ec3dc187b8556dc20ab58e02db4aa78a4
Ad686 <54314203+Ad686@users.noreply.github.com>
2022-05-28 22:43:04 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/app.js",
+    "./src/components/navbar.js"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // 'acm_blue': '#1e6cef',
+        'acm_blue': '#1e3a8a',
+      },
+    },
+  },
+  plugins: [],
+}
